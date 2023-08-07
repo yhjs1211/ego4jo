be5f9cff9fd965cbb1c982fea7f0ae0bf250aab6
yhjs1211 <yhjs1211@gmail.com>
2023-08-07 23:54:13 +0900

    card update

--- src/cards/cards.service.ts
+++ src/cards/cards.service.ts
@@ -1,7 +1,16 @@
 import { Injectable } from '@nestjs/common';
 import { CardRepository } from './card.repository';
+import { CreateCardDTO } from './DTO/create.DTO';
 
 @Injectable()
 export class CardsService {
   constructor(private readonly cardRepository: CardRepository) {}
+
+  getCardDetailById(id: number) {
+    return this.cardRepository.findOneById(id);
+  }
+
+  createCard(data: CreateCardDTO) {
+    return this.cardRepository.createCard(data);
+  }
 }
