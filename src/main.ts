e6273d6810387f1bede1d2c7794885e7f449b38b
kme-developer <electruc0095@gmail.com>
2023-08-09 12:09:41 +0900

    2023.08.09(KJY)
    main.ts에 app.useGlobalPipes 구문 추가
    API.document.ts 내용 일부 수정
    swaggerModule.setup 'api' => 'doc'
    사용자 회원가입 기능 구현

--- src/main.ts
+++ src/main.ts
@@ -2,11 +2,13 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import swagger from './API.document';
 import { HttpExceptionFilter } from './common/exceptions/http-exception.filter';
+import { ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
   // global
+  app.useGlobalPipes(new ValidationPipe());
   app.useGlobalFilters(new HttpExceptionFilter());
 
   // swagger
---
+++ src/users/services/users.service.ts
@@ -0,0 +1,31 @@
+import { Injectable, UnauthorizedException } from '@nestjs/common';
+import { UsersRepository } from '../repositories/users.repository';
+import { UserRequestDto } from '../dto/users.request.dto';
+import * as bcrypt from 'bcrypt';
+
+@Injectable()
+export class UsersService {
+  constructor(private readonly usersRepository: UsersRepository) {}
+
+  async signUp(body: UserRequestDto) {
+    const { email, name, password } = body;
+    const isUserExist = await this.usersRepository.findUserByEmail(email);
+
+    if (isUserExist) {
+      throw new UnauthorizedException('이미 존재하는 사용자 입니다.');
+    }
+
+    const hashedPassword = await bcrypt.hash(password, 10);
+
+    const cat = await this.usersRepository.createUser({
+      email,
+      name,
+      password: hashedPassword,
+    });
+
+    return {
+      statusCode: 201,
+      message: '회원가입 성공',
+    };
+  }
+}
