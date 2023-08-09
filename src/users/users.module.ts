525fa44df3c8eb2b764c65dd86949f5163f29f34
kme-developer <electruc0095@gmail.com>
2023-08-09 14:36:44 +0900

    2023.08.09(KJY)
    사용자 log-in 기능 구현
    이에 따른  folder 추가
    guard => strategy
    현재 사용자 정보 불러오기 기능 구현

--- src/users/users.module.ts
+++ src/users/users.module.ts
@@ -1,12 +1,13 @@
-import { Module } from '@nestjs/common';
+import { Module, forwardRef } from '@nestjs/common';
 import { UsersController } from './controllers/users.controller';
 import { UsersService } from './services/users.service';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { Users } from './users.entity';
 import { UsersRepository } from './repositories/users.repository';
+import { AuthModule } from 'src/auth/auth.module';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([Users])],
+  imports: [TypeOrmModule.forFeature([Users]), forwardRef(() => AuthModule)],
   controllers: [UsersController],
   providers: [UsersService, UsersRepository],
   exports: [UsersService, UsersRepository],
