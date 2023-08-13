eeba7095e57ca5e8acd66986c1d0c6972e3c56a5
Kim Taejin <rlaxowls0504@gmail.com>
2023-08-13 14:22:55 +0900

    board 수정

--- src/board/entity/user_board.entity.ts
+++ src/board/entity/user_board.entity.ts
@@ -1,6 +1,14 @@
 import { IsNumber } from 'class-validator';
 import { Users } from 'src/users/users.entity';
-import { Entity, ManyToOne, OneToMany, PrimaryColumn } from 'typeorm';
+import {
+  CreateDateColumn,
+  DeleteDateColumn,
+  Entity,
+  ManyToOne,
+  OneToMany,
+  PrimaryColumn,
+  UpdateDateColumn,
+} from 'typeorm';
 import { Board } from './board.entity';
 
 @Entity({ schema: 'trello', name: 'user_board' })
@@ -11,9 +19,19 @@ export class User_Board {
   @PrimaryColumn()
   boardId: number;
 
-  @ManyToOne(() => Users, (user) => user.userBoard)
+  @CreateDateColumn()
+  createdAt: Date;
+
+  @UpdateDateColumn()
+  updatedAt: Date;
+
+  @ManyToOne(() => Users, (user) => user.userBoard, {
+    onDelete: 'CASCADE',
+  })
   user: Users;
 
-  @ManyToOne(() => Board, (board) => board.userBoard)
+  @ManyToOne(() => Board, (board) => board.userBoard, {
+    onDelete: 'CASCADE',
+  })
   board: Board;
 }
